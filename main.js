06ea47b7ee45d653ea97e3a81767083f8223d5bc
AratiShahare <shaharearati0@gmail.com>
2022-11-27 18:22:05 +0530

    added

---
+++ main.js
@@ -0,0 +1,50 @@
+
+month=prompt('enter month number')
+if(month==1)
+{
+     alert('1 january')
+}
+else if(month==2)
+{
+     alert('2 february')
+}
+else if(month==3)
+{
+     alert('3 march')
+}
+else if(month==4)
+{
+     alert('4 april')
+}
+else if(month==5)
+{
+     alert('5 may')
+}
+else if(month==6)
+{
+     alert('6 june')
+}
+else if(month==7)
+{
+     alert('7 july')
+}
+else if(month==8)
+{
+     alert('8 august')
+}
+else if(month==9)
+{
+     alert('9 september')
+}
+else if(month==10)
+{
+     alert('10 october')
+}
+else if(month==11)
+{
+     alert('11 november')
+}
+else(month==12)
+{
+     alert('12 december')
+}
